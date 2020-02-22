b672d083541b60f861f7e144bf80eb590bd8baf7
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 04:48:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,10 @@
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def happy_birthday(birthday_kids)
+birthday_kids.each do |kids_name, age|
+  if age > 12 
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end
