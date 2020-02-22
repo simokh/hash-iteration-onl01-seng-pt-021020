6b6d93e4259e5bc9787cb1c3bbcf6437662d3c63
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 04:33:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,7 @@
 # }
 
 def select_winner(passengers)
-  passengers.each do |key, value|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
